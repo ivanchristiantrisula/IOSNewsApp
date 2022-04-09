2ec5dafae8e00d6449f18cb408e7762781a4501f
Ivan Christian <hi@ivanchristian.me>
2022-04-09 18:56:58 +0700

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ivan Christian on 09/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
